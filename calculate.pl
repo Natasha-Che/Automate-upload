cd745bcedf82e6688f606fe575f04efb9a92f382
Natasha Che <xiaohoche@hotmail.com>
2013-12-15 22:46:50 -0800

    Update calculate.pl

--- calculate.pl
+++ calculate.pl
@@ -37,3 +37,43 @@ $rev_count=0;
 open(FILE1,"tq_browser_os_distribution1");
 while (<FILE1>)
 { $line=$_;
+chomp $line;
+  (@a)=split /\t/, $line;
+  $per_req=$a[4]/$total_req;
+  $per_imp=$a[5]/$total_imp;
+  $per_rev=$a[6]/$total_rev;
+  $cul_req=$cul_req+$a[4];
+  $cul_imp=$cul_imp+$a[5];
+  $cul_rev=$cul_rev+$a[6];
+  $cul_per_req=$cul_req/$total_req;
+  $cul_per_imp=$cul_imp/$total_imp;
+  $cul_per_rev=$cul_rev/$total_rev;
+  $req_count=$a[4];
+  for ($i = -3; $i > -1 * length($req_count); $i -= 4)
+  {substr($req_count, $i,0)=',';
+}
+  $imp_count=$a[5];
+  for ($i = -3; $i > -1 * length($imp_count); $i -= 4)
+  {substr($imp_count, $i,0)=',';
+}
+
+  $rev_count=int($a[6]);
+   for ($i = -3; $i > -1 * length($rev_count); $i -= 4)
+  {substr($rev_count, $i,0)=',';
+}
+  $per_req1=int($per_req*10000000)/100000;
+  $per_imp1=int($per_imp*10000000)/100000;
+  $per_rev1=int($per_rev*10000000)/100000;
+  $cul_per_req1=int($cul_per_req*10000000)/100000;
+  $cul_per_imp1=int($cul_per_imp*10000000)/100000;
+  $cul_per_rev1=int($cul_per_rev*10000000)/100000;
+  print $a[0]."\t".$a[1]."\t".$a[2]."\t".$a[3]."\t".$req_count."\t".$per_req1."%\t".$cul_per_req1."%\t".$imp_count."\t".$per_imp1."%\t".$cul_per_imp1."%\t".$rev_cou\
+nt."\t".$per_rev1."%\t".$cul_per_rev1."%"."\n";
+
+}
+
+close(FILE1);
+
+
+
+
